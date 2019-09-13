1595a9ed80ef09eee6e671869034e90701d42bbb
Aishvaria08 <alaturkar@mail.sfsu.edu>
2019-09-13 16:41:25 -0700

    Initializing repo

---
+++ src/main/java/com/tradingcompany/server/Trading/company/server/TradingCompanyServerApplication.java
@@ -0,0 +1,13 @@
+package com.tradingcompany.server.Trading.company.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingCompanyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingCompanyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tradingcompany/server/Trading/company/server/TradingCompanyServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tradingcompany.server.Trading.company.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TradingCompanyServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
